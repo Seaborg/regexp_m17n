17bdeaf59c5e8c6e216b03fe0778bbec311913d1
Juliusz Filipowski <filipowj@tchpc.tcd.ie>
2014-08-06 23:44:22 +0100

    Refactor!

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,10 +1,5 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str = str.force_encoding("UTF-8")
-    if (str =~ /^.+$/)
-      true
-    else
-      false
-    end
+    str.force_encoding("UTF-8") =~ /^.+$/
   end
 end

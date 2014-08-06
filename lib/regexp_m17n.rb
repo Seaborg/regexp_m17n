2647a0166e00f6f0c6d36992d45d1751b35417d8
Juliusz Filipowski <filipowj@tchpc.tcd.ie>
2014-08-06 23:06:10 +0100

    I've solved it!

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,10 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str = str.force_encoding("UTF-8")
+    if (str =~ /^.+$/)
+      true
+    else
+      false
+    end
   end
 end
